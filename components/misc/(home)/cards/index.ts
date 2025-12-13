b670c7adb1048b0817d9c05aa9d9df1a2cbca143
Tanay Kumar <tanay25000@gmail.com>
2025-12-13 19:35:53 +0530

    fixing vercel8

--- components/misc/(home)/cards/index.ts
+++ components/misc/(home)/cards/index.ts
@@ -1,12 +1,5 @@
 // components/misc/(home)/cards/index.ts
-
-export * from "./animelink-card";
-export * from "./books-card";
 export * from "./current-time";
-export * from "./gh-link";
 export * from "./links-card";
-export * from "./locate-me";
-export * from "./music-card";
 export * from "./stacks-card";
-export * from "./wakatime-stats";
 // REMOVED: export * from "./contact-card";
